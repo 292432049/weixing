4b222424ca4fa5b6692d806f5888256901c5ef87
dengdongming <ddm@fuhengkeji.com>
2019-03-04 14:46:30 +0800

    bb

--- house_festival/src/api/asistancePage.js
+++
@@ -1,9 +0,0 @@
-import { fetch } from '@/utils/fetch'
-
-/* 好友助力 */
-export function friendsHelpApi (params = {}) {
-  return fetch('/api/coupon', {
-    method: 'helpCustomer',
-    params
-  }, 'post')
-}
